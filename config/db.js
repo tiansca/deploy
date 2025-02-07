4d36f9784c9b56667ddad672ae793573ae6d9797
tianshicong <tianshicong@zbintel.net>
2025-02-07 16:30:51 +0800

    数据库配置

--- config/db.js
+++ config/db.js
@@ -2,8 +2,15 @@
  * Created by administrator on 2019/10/31.
  */
 var mongoose = require('mongoose');
-// var url ="mongodb://root:tianshicong@localhost:27017/deploy-dev";
-var url ="mongodb://root:tianshicong@192.168.11.25:27017/deploy-remote";
+var url ="mongodb://root:tianshicong@localhost:27017/deploy-dev";
+// var url ="mongodb://root:tianshicong@192.168.11.25:27017/deploy-remote";
+if (process.env.DOCKER === 'yes') {
+    if (process.env.MOMGO_DOCKER === 'yes') {
+        url = "mongodb://root:tianshicong@mongo:27017/deploy"; // docker容器内mongo服务
+    } else {
+        url ="mongodb://root:tianshicong@192.168.11.25:27017/deploy-remote"; // 主机上mongo服务
+    }
+}
 mongoose.set('useCreateIndex', true)
 mongoose.set('useFindAndModify', false)
 mongoose.connect(url);
