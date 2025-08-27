6a655f22fe1fc3926582f779cfbdbb8c8fe565d6
tianshicong <tianshicong@zbintel.net>
2025-08-27 19:01:58 +0800

    #fix:isError

--- utils/deploy_work.js
+++ utils/deploy_work.js
@@ -227,7 +227,7 @@ function getStat(path){
 
 const isError = (str) => {
     // console.log('msg', str)
-    if (str.indexOf('err') !== -1 || str.indexOf('ERR') !== -1 || str.indexOf('killed') !== -1 || str.indexOf('Killed') !== -1) {
+    if (str.indexOf('err ') !== -1 || str.indexOf('ERR ') !== -1 || str.indexOf('err:') !== -1 || str.indexOf('ERR:') !== -1 || str.indexOf('error') !== -1 || str.indexOf('Error') !== -1 || str.indexOf('killed') !== -1 || str.indexOf('Killed') !== -1) {
         return Promise.reject('错误，终止')
     } else {
         return Promise.resolve('')
